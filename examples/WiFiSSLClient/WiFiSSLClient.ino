a8bfa061e1e2de9c8746c77170a5badd623c31de
ladyada <limor@ladyada.net>
2019-05-12 18:23:01 -0400

    set up all the pin options

--- examples/WiFiSSLClient/WiFiSSLClient.ino
+++ examples/WiFiSSLClient/WiFiSSLClient.ino
@@ -14,13 +14,38 @@ last revision November 2015
 #include <WiFiNINA.h>
 
 // Configure the pins used for the ESP32 connection
-#if !defined(SPIWIFI_SS)  // if the wifi definition isnt in the board variant
+#if defined(ADAFRUIT_FEATHER_M4_EXPRESS) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_AVR_FEATHER32U4) || defined(ARDUINO_NRF52840_FEATHER)
+  // Configure the pins used for the ESP32 connection
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS    13   // Chip select pin
+  #define ESP32_RESETN  12   // Reset pin
+  #define SPIWIFI_ACK   11   // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif defined(ARDUINO_AVR_FEATHER328P) 
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS     4   // Chip select pin
+  #define ESP32_RESETN   3   // Reset pin
+  #define SPIWIFI_ACK    2   // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif defined(TEENSYDUINO) 
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS     5   // Chip select pin
+  #define ESP32_RESETN   6   // Reset pin
+  #define SPIWIFI_ACK    9   // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif defined(ARDUINO_NRF52832_FEATHER )
+  #define SPIWIFI       SPI  // The SPI port
+  #define SPIWIFI_SS    16  // Chip select pin
+  #define ESP32_RESETN  15  // Reset pin
+  #define SPIWIFI_ACK    7  // a.k.a BUSY or READY pin
+  #define ESP32_GPIO0   -1
+#elif !defined(SPIWIFI_SS)  // if the wifi definition isnt in the board variant
   // Don't change the names of these #define's! they match the variant ones
-  #define SPIWIFI     SPI
-  #define SPIWIFI_SS    10  // Chip select pin
-  #define SPIWIFI_ACK   7   // a.k.a BUSY or READY pin
-  #define ESP32_RESETN  5   // Reset pin
-  #define ESP32_GPIO0   -1  // Not connected
+  #define SPIWIFI       SPI
+  #define SPIWIFI_SS    10   // Chip select pin
+  #define SPIWIFI_ACK    7   // a.k.a BUSY or READY pin
+  #define ESP32_RESETN   5   // Reset pin
+  #define ESP32_GPIO0   -1   // Not connected
 #endif
 
 #include "arduino_secrets.h" 
